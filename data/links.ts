e3d13fdf01c358a1c12a758fe76b15d067eed01c
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-09-03 18:07:16 +0530

    seeding the database 🌱

--- data/links.ts
+++ data/links.ts
@@ -2,7 +2,7 @@ export const links = [
   {
     category: "Open Source",
     description: "Fullstack React framework",
-    id: 1,
+    id: '1',
     imageUrl: "https://github.com/vercel.png",
     title: "Next.js",
     url: "https://nextjs.org",
@@ -10,16 +10,15 @@ export const links = [
   {
     category: "Open Source",
     description: "Next Generation ORM for TypeScript and JavaScript",
-    id: 2,
+    id: '2',
     imageUrl: "https://github.com/prisma.png",
-
     title: "Prisma",
     url: "https://prisma.io",
   },
   {
     category: "Open Source",
     description: "Utility-fist css framework",
-    id: 3,
+    id: '3',
     imageUrl: "https://github.com/tailwindlabs.png",
     title: "TailwindCSS",
     url: "https://tailwindcss.com",
@@ -27,7 +26,7 @@ export const links = [
   {
     category: "Open Source",
     description: "GraphQL implementation ",
-    id: 4,
+    id: '4',
     imageUrl: "https://www.apollographql.com/apollo-home.jpg",
     title: "Apollo GraphQL",
     url: "https://apollographql.com",
---
+++ prisma/migrations/20230902190404_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_LinkToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_LinkToUser_AB_unique" ON "_LinkToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_LinkToUser_B_index" ON "_LinkToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_LinkToUser" ADD CONSTRAINT "_LinkToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Link"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LinkToUser" ADD CONSTRAINT "_LinkToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
