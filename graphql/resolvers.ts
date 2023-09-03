98ba0db8e40512fa102a69a967965830e4820f0a
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-09-04 00:07:31 +0530

    feat: 🚧 graphQL integration 🚧

---
+++ graphql/resolvers.ts
@@ -0,0 +1,32 @@
+export const resolvers = {
+    Query: {
+      links: () => {
+        return [
+          {
+            category: 'Open Source',
+            description: 'Fullstack React framework',
+            id: 1,
+            imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
+            title: 'Next.js',
+            url: 'https://nextjs.org',
+          },
+          {
+            category: 'Open Source',
+            description: 'Next Generation ORM for TypeScript and JavaScript',
+            id: 2,
+            imageUrl: 'https://www.prisma.io/images/og-image.png',
+            title: 'Prisma',
+            url: 'https://www.prisma.io',
+          },
+          {
+            category: 'Open Source',
+            description: 'GraphQL implementation',
+            id: 3,
+            imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
+            title: 'Apollo GraphQL',
+            url: 'https://apollographql.com',
+          },
+        ]
+      },
+    },
+  }
---
+++ graphql/schema.ts
@@ -0,0 +1,15 @@
+export const typeDefs = `
+  type Link {
+    id: ID
+    title: String
+    description: String
+    url: String
+    category: String
+    imageUrl: String
+    users: [String]
+  }
+
+  type Query {
+    links: [Link]!
+  }
+`
